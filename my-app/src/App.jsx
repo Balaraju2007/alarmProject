889eeb38604d7f0d57d190b783dd28bd190d8b24
balaraju <balarajumukalla3@gmail.com>
2025-08-20 00:10:41 +0530

    add tailwind css

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,16 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Body from './assets/Body.jsx'
+
+
+const Header = () => {
+  return (
+      <header>
+       <h1 className='p-4 text-center text-2xl w-[50%] border border-blue-700 m-auto'> Alarm </h1> 
+      </header>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <header className="text-[400%] font-bold text-blue-600 mb-4">
-        Alarm
-      </header>
+    <main className='text-center h-full mt-4 '>
+      <div className='bg-color-blue-500 border border-black-500 w-[40%] m-auto h-[30rem] p-4 rounded-2xl'>
+          <Header />
+          <Body />
+      </div>
+      </main>
     </>
   )
 }
---
+++ my-app/src/assets/Body.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div
+     className='text-center bg-color-red-500 border border-red-500 w-[50%] m-auto 
+     h-[23rem] mt-4 p-4 rounded-2xl overflow-auto scrollbar-hide'>
+        bodyy---------
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+Section 1.10.32 of "de Finibus Bo"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+Section 1.10.32 of "de Finibus Bo
+    </div>
+  )
+}
+
+export default Body
