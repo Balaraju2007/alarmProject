a48e6dfa1d3c19d6bfda076c1c1c781d68fc5dfb
balaraju <balarajumukalla3@gmail.com>
2025-08-20 00:25:33 +0530

    add hour using map function

--- my-app/src/assets/Body.jsx
+++ my-app/src/assets/Body.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
 const Body = () => {
+    const hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12];
   return (
     <div
      className='text-center bg-color-red-500 border border-red-500 w-[50%] m-auto 
      h-[23rem] mt-4 p-4 rounded-2xl overflow-auto scrollbar-hide'>
         bodyy---------
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-Section 1.10.32 of "de Finibus Bo"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-Section 1.10.32 of "de Finibus Bo
+       <div className='text-2xl font-bold text-blue-600'>  {hour.map((h, index) => (
+           
+               <p  key={index} >{h} : </p> 
+       
+        ))}
+             </div>
     </div>
   )
 }
